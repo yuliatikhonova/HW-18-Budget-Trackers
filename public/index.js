f6ba81be9f2b3f3fecf73edfc66bd8aba2e919b6
yulia tikhonova <yuliatikhonova@outlook.com>
2020-08-08 20:16:42 -0700

    adding the code for indexdb

--- public/index.js
+++ public/index.js
@@ -1,6 +1,76 @@
 let transactions = [];
 let myChart;
 
+function checkForIndexedDb() {
+  if (!window.indexedDB) {
+    console.log("Your browser doesn't support a stable version of IndexedDB.");
+    return false;
+  }
+  return true;
+}
+
+function useIndexedDb(databaseName, storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open(databaseName, 1);
+    let db,
+      tx,
+      store;
+
+    request.onupgradeneeded = function (e) {
+      const db = request.result;
+      db.createObjectStore(storeName, { keyPath: "_id" });
+    };
+
+    request.onerror = function (e) {
+      console.log("There was an error");
+    };
+
+    request.onsuccess = function (e) {
+      db = request.result;
+      tx = db.transaction(storeName, "readwrite");
+      store = tx.objectStore(storeName);
+
+      db.onerror = function (e) {
+        console.log("error");
+      };
+      if (method === "put") {
+        store.put(object);
+      } else if (method === "get") {
+        const all = store.getAll();
+        all.onsuccess = function () {
+          resolve(all.result);
+        };
+      } else if (method === "delete") {
+        store.delete(object._id);
+
+      } else if (method === "add") {
+        const objRequest = store.add(object);
+        objRequest.onsuccess = function (event) {
+          console.log("success", event);
+        }
+        objRequest.onerror = function (event) {
+          console.log("error", event);
+        }
+      } 
+
+      tx.oncomplete = function () {
+        db.close();
+      };
+    };
+  });
+};
+
+function saveRecord(transaction) {
+  if (checkForIndexedDb()) {
+    useIndexedDb("budget", "pending", "add", transaction)
+      .then(
+        () => {
+          console.log("added to DB");
+        }
+      );
+  };
+};
+
 fetch("/api/transaction")
   .then(response => {
     return response.json();
@@ -66,14 +136,14 @@ function populateChart() {
 
   myChart = new Chart(ctx, {
     type: 'line',
-      data: {
-        labels,
-        datasets: [{
-            label: "Total Over Time",
-            fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
+    data: {
+      labels,
+      datasets: [{
+        label: "Total Over Time",
+        fill: true,
+        backgroundColor: "#6666ff",
+        data
+      }]
     }
   });
 }
@@ -111,7 +181,7 @@ function sendTransaction(isAdding) {
   populateChart();
   populateTable();
   populateTotal();
-  
+
   // also send to server
   fetch("/api/transaction", {
     method: "POST",
@@ -121,33 +191,36 @@ function sendTransaction(isAdding) {
       "Content-Type": "application/json"
     }
   })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      }
+      else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+
       // clear form
       nameEl.value = "";
       amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
-
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
+    });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+
+
+document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
 };
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector("#sub-btn").onclick = function () {
+
   sendTransaction(false);
 };
